da625ee5a4e9d6bf45152a634d8f8d64351e1585
DEEP KAKADIYA <142491093+deepkakadiya7@users.noreply.github.com>
2024-04-07 18:37:03 +0530

    Add  Features in switch i.cpp

--- Source_code.cpp
+++ Source_code.cpp
@@ -3,5 +3,30 @@ using namespace std;
 
 int main(){
 
+   char choice;
+ 
+
+    do {
+        cout << "1 --> Add Contact\n";
+        cout << "2 --> Show All Contacts\n";
+        cout << "3 --> Search Contact\n";
+        cout << "4 --> Edit Contact\n";
+        cout << "5 --> Delete Contact\n";
+        cout << "6 --> Sort Contacts\n";
+        cout << "0 --> Exit\n";
+        cout << "Enter your choice: ";
+        cin >> choice;
+        cin.ignore(); // Clear the newline character from the buffer
+
+        switch(choice) {
+            case '1':
+                break;
+           
+            
+            default:
+                
+                break;
+        }
+    } while (choice != '0');
 return 0;
 }
